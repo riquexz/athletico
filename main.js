a359dca9db1af857713efaa24c70553c5f236b07
riquexz <161594551+riquexz@users.noreply.github.com>
2024-03-25 08:48:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,6 @@ for(let i=0;i <botoes.length;i++){
         textos[i].classList.add("ativo");
     }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-03-25T00:00:00");
+let tempoAtual = new Date();
